06e4cd6a990017f0c002170ca2bfd4bac449fd1e
gomescarlosdev <carlos.henrique.pro@outlook.com>
2022-09-20 22:46:07 -0300

    feat(Math Calc): :sparkles: Nova funcionalidade de multiplicacao
    Realizara a multiplicacao entre 2 termos.

--- math.go
+++ math.go
@@ -3,8 +3,13 @@ import "fmt"
 
 func main(){
     fmt.Println(Sum(10,10))
+    fmt.Println(Multi(10,10))
 }
 
 func Sum(a int, b int) int {
     return a + b
+}
+
+func Multi(a int, b int) float64 {
+    return a * b
 }
--- math_test.go
+++ math_test.go
@@ -3,9 +3,17 @@ package main
 import "testing"
 
 func TestSum(t *testing.T){
-    total := Sum(15,18)
+    total := Sum(15, 15)
 
     if total != 30 {
 	    t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
+    }
+}
+
+func TestMulti(t *testing.T){
+    total := Sum(10, 15)
+
+    if total != 150 {
+	    t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 150)
     }
 }
