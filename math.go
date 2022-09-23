d53e05697ecc047427bb408a167c361c9da44843
gomescarlosdev <carlos.henrique.pro@outlook.com>
2022-09-22 22:42:49 -0300

    fix(Math Calc): :fire: erros de compilacao
    removido os erros de compilacao

--- math.go
+++ math.go
@@ -1,15 +1,16 @@
 package main
+
 import "fmt"
 
-func main(){
-    fmt.Println(Sum(10,10))
-    fmt.Println(Multi(10,10))
+func main() {
+	fmt.Println(Sum(10, 10))
+	fmt.Println(Multi(10, 10))
 }
 
 func Sum(a int, b int) int {
-    return a + b
+	return a + b
 }
 
 func Multi(a int, b int) int {
-    return a * b
+	return a * b
 }
