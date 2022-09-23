ce1e5c0fdea66d1aecca1db313fbac16190535e7
gomescarlosdev <carlos.henrique.pro@outlook.com>
2022-09-22 22:28:17 -0300

    fix: :bug: Fix teste unitario
    Corrigido o valor de retorno do teste unitario

--- main/math_test.go
+++ main/math_test.go
@@ -2,18 +2,18 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T){
-    total := Sum(15, 15)
+func TestSum(t *testing.T) {
+	total := Sum(15, 15)
 
-    if total != 30 {
-	    t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
-    }
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 30)
+	}
 }
 
-func TestMulti(t *testing.T){
-    total := Multi(10, 16)
+func TestMulti(t *testing.T) {
+	total := Multi(10, 15)
 
-    if total != 150 {
-	    t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 150)
-    }
+	if total != 150 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado %d", total, 150)
+	}
 }
